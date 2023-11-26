23b3c4935596c9c99ead7c083f0ce35315fe0280
Juan Rodriguez <jp.rodriguez22@uniandes.edu.co>
2023-11-25 22:53:18 -0500

    12-13-16 finalizados en cypress

---
+++ e2e/cypress/e2e/ghost-tests/data_generation/dinamico/ID13.cy.js
@@ -0,0 +1,49 @@
+/// <reference types="cypress" />
+const loginPage = require('../../../../../page_objects/cypress/loginPage.json')
+const pagesPage = require('../../../../../page_objects/cypress/pagesPage.json')
+const settingsPage = require('../../../../../page_objects/cypress/settingsPage.json')
+const homePage = require('../../../../../page_objects/cypress/homePage.json')
+const properties = require('../../../../../properties.json')
+const MockarooInterface = require('../../../../../page_objects/cypress/mockarooInterface')
+
+context('#13 Crear una página nueva y agregarla en el navbar', () => {
+  // Given I login to my website
+  beforeEach(name, () => {
+    cy.viewport('macbook-16'),
+    cy.visit(properties.GHOST5),
+    cy.get(loginPage.elementEmail).type(properties.LOGINEMAIL),
+    cy.get(loginPage.elementPassword).type(properties.LOGINPASSWORD),
+    cy.get(loginPage.loginButton).click(),
+    cy.wait(1000)
+})
+  // When I create a new page
+  it('Crear una nueva página', () => {
+    const mockInterface = new MockarooInterface()
+    cy.wrap(mockInterface.dynamicInitializeUsers()).then(mockUser => {
+      const user1 = mockInterface.getRandom(mockUser)
+      const user2 = mockInterface.getRandom(mockUser)
+      cy.get(pagesPage.panelButton).click()    
+      cy.get(pagesPage.newPageButton).click()
+      cy.wait(1000)
+      const pageTitle = user2.paragraph.replace(/[@.,]/g, "") // And add a dynamic random age to the page title
+      cy.get(pagesPage.newPagetitleField).type(pageTitle)
+      cy.get(pagesPage.newPagePostField).type(user1.biography)
+      cy.wait(3000)
+      cy.get(pagesPage.publishButton).click()
+      cy.get(pagesPage.continueButton).click()
+      cy.get(pagesPage.confirmButton).click()
+
+      cy.visit(properties.GHOST5)
+      cy.visit(settingsPage.navigationSettingsURL)
+      cy.wait(1000)
+      cy.get(settingsPage.newNavInput).filter(':visible').type(pageTitle)
+      cy.get(settingsPage.newNavUrl).filter(':visible').last().type(pageTitle.toLowerCase().replace(/[@. ]/g, "-")).type('{enter}')
+      cy.wait(1000)
+      cy.get(settingsPage.newNavOkButton).click()
+      // Then I navigate to the homepage and select the element in the navbar
+      cy.visit(homePage.url)
+      cy.wait(2000)
+      cy.get('.nav-'+pageTitle.toLowerCase().replace(/[@. ]/g, "-")).click({ force: true })
+    })
+  })
+})
---
+++ e2e/page_objects/cypress/mockarooInterface.js
@@ -0,0 +1,57 @@
+const fetch = require('node-fetch')
+// Importación de mockups priori
+const prioriUsers = require("../../cypress/e2e/ghost-tests/data_generation/priori/ghost_users.json")
+const prioriHex = require("../../cypress/e2e/ghost-tests/data_generation/priori/random_hex.json")
+const prioriPosts = require("../../cypress/e2e/ghost-tests/data_generation/priori/ghost_posts.json")
+const prioriMembers = require("../../cypress/e2e/ghost-tests/data_generation/priori/ghost_members.json")
+
+// Importación de mockups dinámicos
+const mockarooSettings = require("../../cypress/e2e/ghost-tests/data_generation/dinamico/APIs.json");
+
+class MockarooInterface{
+    async dynamicInitializeUsers(){
+        let jsonArray = await fetch(mockarooSettings.ghost_user);
+        let response = await jsonArray.json();
+        return await response;
+    }
+
+    async dynamicInitializeHex(){
+        let jsonArray = await fetch(mockarooSettings.random_hex);
+        let response = await jsonArray.json();
+        return await response;
+    }
+
+    async dynamicInitializePosts(){
+        let jsonArray = await fetch(mockarooSettings.ghost_post);
+        let response = await jsonArray.json();
+        return await response;
+    }
+
+    async dynamicInitializeMembers(){
+        let jsonArray = await fetch(mockarooSettings.ghost_member);
+        let response = await jsonArray.json();
+        return await response;
+    }
+
+    prioriInitializeUsers(){
+        return prioriUsers;
+    }
+
+    prioriInitializeHex(){
+        return prioriHex;
+    }
+
+    prioriInitializePosts(){
+        return prioriPosts;
+    }
+
+    prioriInitializeMembers(){
+        return prioriMembers;
+    }
+
+    getRandom(data){
+        return data[Math.floor(Math.random()*data.length)];
+    }
+}
+
+module.exports = MockarooInterface;
